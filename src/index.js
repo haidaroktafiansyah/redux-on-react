3329e9d069ad288f9873ede1f24a573b0123985d
Haidar Oktafiansyah <54706023+haidaroktafiansyah@users.noreply.github.com>
2021-03-15 08:55:20 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Table from './container/Table';
+import CreateTodo from './container/CreateTodo';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+ReactDOM.render(<Provider store={store}>
+  <CreateTodo />
+  <Table />
+</Provider>
+  , document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
